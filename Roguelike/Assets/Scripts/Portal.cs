cd1759bb60cddf5a2ca15c670c54bfe055bd0be4
최은성 <csx1245@gmail.com>
2024-03-18 20:48:41 +0900

    Feat : 포탈 추가
    간단한 씬 이동
    + 플레이어 태그 추가, rigidbody / collider 추가

---
+++ Roguelike/Assets/Scripts/Portal.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Scene nowScene = SceneManager.GetActiveScene();
+
+        if (other.gameObject.CompareTag("Player"))   //오브젝트 태그가 Player일시 씬 불러오기
+        {
+            Debug.Log("1");
+            switch(nowScene.buildIndex)
+            {
+                case (0) :
+                    SceneManager.LoadScene(1);
+                    break;
+
+                case (1) :
+                    SceneManager.LoadScene(0);
+                    break;
+            }
+        }
+
+    }
+    
+}
