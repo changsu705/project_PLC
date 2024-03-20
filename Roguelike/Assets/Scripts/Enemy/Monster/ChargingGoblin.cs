4dd0c0e2cb772a4c468b050092040a3bbdf51e0b
changwoomin <changwoomin426@gmail.com>
2024-03-20 19:35:11 +0900

    [Feat] 애니메이션 임시용
    24.03.20(수)

---
+++ Roguelike/Assets/Scripts/Enemy/Monster/ChargingGoblin.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChargingGoblin : Enemy
+{
+    
+    private void Awake()
+    {
+        rb=GetComponent<Rigidbody>();
+        nav=GetComponent<NavMeshAgent>();
+        anim=GetComponent<Animator>();
+    }
+
+    
+
+    public override IEnumerator Attack()
+    {
+        isChase = false;
+        isAttack = true;
+        anim.SetBool("isAttack", true);
+        
+        yield return new WaitForSeconds(0.1f);
+        rb.AddForce(transform.forward * 20, ForceMode.Impulse);
+        meleeArea.enabled= true;
+        
+        yield return new WaitForSeconds(0.5f);
+        rb.velocity = Vector3.zero;
+        meleeArea.enabled= false;
+        
+        yield return new WaitForSeconds(2f);       
+        
+        isChase = true;
+        isAttack = false;
+        anim.SetBool("isAttack", false);
+       
+    }
+}
