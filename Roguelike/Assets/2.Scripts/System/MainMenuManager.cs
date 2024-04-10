e1eee347f6ae9f39be6483f09931655f5a4f3ec1
eunseong <csx1245@gmail.com>
2024-04-10 17:47:58 +0900

    Feat : 메인메뉴 추가
    '24.04.10. 수

---
+++ Roguelike/Assets/2.Scripts/System/MainMenuManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeSpeed = 1.5f;
+
+    private bool isFading = false;
+
+    public void StartGame()
+    {
+        if (!isFading)
+        {
+            isFading = true;
+            StartCoroutine(FadeIn());
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        Color color = fadeImage.color;
+        while (fadeImage.color.a < 1)
+        {
+            color.a += fadeSpeed * Time.deltaTime;
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("Scene2");
+    }
+}
