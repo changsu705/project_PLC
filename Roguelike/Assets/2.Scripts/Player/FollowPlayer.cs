c1d8c59ce83f8057e75bd7acfa7f7823f8239f7d
changwoomin <changwoomin426@gmail.com>
2024-04-16 00:42:52 +0900

    feat : 미니맵 구현

---
+++ Roguelike/Assets/2.Scripts/Player/FollowPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    public Vector3 offset;
+
+    void Update()
+    {
+        if(player!=null)
+        {
+            transform.position = player.position + offset;
+        }
+    }
+}
