fe5a5d02386a6abb332fd9954523533df9a164d1
eunseong <csx1245@gmail.com>
2024-04-18 09:25:48 +0900

    Style : 코드 주석해제
    '24.04.18 목

--- Roguelike/Assets/2.Scripts/System/GameManager.cs
+++ Roguelike/Assets/2.Scripts/System/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject portalObject;
 
     public GameObject escButton;
     public GameObject backToGameButton;
@@ -21,12 +22,35 @@ public class GameManager : MonoBehaviour
     {
         Time.timeScale = 1;
         escButton.SetActive(false);
-        
+
+        if (swordGoblinSpawnPoints.Length > 0)
+        {
+            SpawnGoblin(GoblinType.Sword);
+        }
+
+        if (bowGoblinSpawnPoints.Length > 0)
+        {
+            SpawnGoblin(GoblinType.Bow);
+        }
+
+        if (shieldGoblinSpawnPoints.Length > 0)
+        {
+            SpawnGoblin(GoblinType.Shield);
+        }
     }
 
     private void Update()
     {
         onKeyESC();
+
+        GameObject[] noEnemy = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (noEnemy.Length == 0)
+        {
+            portalObject.SetActive(true);
+        }
+        else
+            portalObject.SetActive(false);
     }
 
     private void onKeyESC()
@@ -47,84 +71,53 @@ public class GameManager : MonoBehaviour
             }
         
     }
-    
-   
-    
-   
 
 
-    // public enum GoblinType { Sword, Bow, Shield };
-    //
-    // public GameObject swordGoblinPrefab;
-    // public GameObject bowGoblinPrefab;
-    // public GameObject shieldGoblinPrefab;
-    //
-    // public Transform[] swordGoblinSpawnPoints;
-    // public Transform[] bowGoblinSpawnPoints;
-    // public Transform[] shieldGoblinSpawnPoints;
-    //
-    // public void SpawnGoblin(GoblinType goblinType)
-    // {
-    //     GameObject goblinPrefab = null;
-    //     Transform[] spawnPoints = null;
-    //
-    //     switch (goblinType)
-    //     {
-    //         case GoblinType.Sword:
-    //             goblinPrefab = swordGoblinPrefab;
-    //             spawnPoints = swordGoblinSpawnPoints;
-    //             break;
-    //         case GoblinType.Bow:
-    //             goblinPrefab = bowGoblinPrefab;
-    //             spawnPoints = bowGoblinSpawnPoints;
-    //             break;
-    //         case GoblinType.Shield:
-    //             goblinPrefab = shieldGoblinPrefab;
-    //             spawnPoints = shieldGoblinSpawnPoints;
-    //             break;
-    //     }
-    //
-    //     if (goblinPrefab != null && spawnPoints != null && spawnPoints.Length > 0)
-    //     {
-    //         foreach (Transform spawnPoint in spawnPoints)
-    //         {
-    //             Instantiate(goblinPrefab, spawnPoint.position, Quaternion.identity);
-    //         }
-    //     }
-    // }
-    //
-    // private void Start()
-    // {
-    //     if (swordGoblinSpawnPoints.Length > 0)
-    //     {
-    //         SpawnGoblin(GoblinType.Sword);
-    //     }
-    //
-    //     if (bowGoblinSpawnPoints.Length > 0)
-    //     {
-    //         SpawnGoblin(GoblinType.Bow);
-    //     }
-    //
-    //     if (shieldGoblinSpawnPoints.Length > 0)
-    //     {
-    //         SpawnGoblin(GoblinType.Shield);
-    //     }
-    // }
-    //
-    // public GameObject portalObject;
-    // private void Update()
-    // {   //적이 없을때 포탈 열리게
-    //     GameObject[] noEnemy = GameObject.FindGameObjectsWithTag("Enemy");
-    //
-    //     if (noEnemy.Length == 0)
-    //     {
-    //         portalObject.SetActive(true);
-    //     }
-    //     else
-    //         portalObject.SetActive(false);
-    // }
 
-    
+
+
+
+    public enum GoblinType { Sword, Bow, Shield };
+
+    public GameObject swordGoblinPrefab;
+    public GameObject bowGoblinPrefab;
+    public GameObject shieldGoblinPrefab;
+
+    public Transform[] swordGoblinSpawnPoints;
+    public Transform[] bowGoblinSpawnPoints;
+    public Transform[] shieldGoblinSpawnPoints;
+
+    public void SpawnGoblin(GoblinType goblinType)
+    {
+        GameObject goblinPrefab = null;
+        Transform[] spawnPoints = null;
+
+        switch (goblinType)
+        {
+            case GoblinType.Sword:
+                goblinPrefab = swordGoblinPrefab;
+                spawnPoints = swordGoblinSpawnPoints;
+                break;
+            case GoblinType.Bow:
+                goblinPrefab = bowGoblinPrefab;
+                spawnPoints = bowGoblinSpawnPoints;
+                break;
+            case GoblinType.Shield:
+                goblinPrefab = shieldGoblinPrefab;
+                spawnPoints = shieldGoblinSpawnPoints;
+                break;
+        }
+
+        if (goblinPrefab != null && spawnPoints != null && spawnPoints.Length > 0)
+        {
+            foreach (Transform spawnPoint in spawnPoints)
+            {
+                Instantiate(goblinPrefab, spawnPoint.position, Quaternion.identity);
+            }
+        }
+    }
+
+
     #region UI
 
     /// <summary>
