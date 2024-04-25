e62099525a1a4e2ed6356abbced0051a070c9415
changwoomin <changwoomin426@gmail.com>
2024-04-25 23:36:30 +0900

    fix : 폭발열매 에너미에게 데미지 주기 적용

--- Roguelike/Assets/2.Scripts/System/FieldObjects/BombFruit.cs
+++ Roguelike/Assets/2.Scripts/System/FieldObjects/BombFruit.cs
@@ -1,10 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class BombFruit : MonoBehaviour
 {
     [SerializeField] private float force;
     [SerializeField] private float explosionRadius;
-    [SerializeField] private float damage;
+    public float damage;
     [SerializeField] private GameObject explosionFlowerPrefab;
     private Rigidbody rigid;
     private AudioManager audioManager;
@@ -26,6 +27,7 @@ public class BombFruit : MonoBehaviour
         }
         else if (rigid.useGravity)
         {
+            //StartCoroutine(nameof(Explosion));
             BOOM();
         }
     }
@@ -39,6 +41,7 @@ public class BombFruit : MonoBehaviour
         {
             if (hitCollider.CompareTag("Enemy"))
             {
+                hitCollider.GetComponent<Enemy>().currentHp -= damage;
                 Debug.Log("Hit");
             }
         }
@@ -51,5 +54,20 @@ public class BombFruit : MonoBehaviour
         }
 
         Destroy(gameObject);
+    }
+
+    IEnumerator Explosion()
+    {
+        RaycastHit [] rayHits =Physics.SphereCastAll(transform.position,15,Vector3.up,0f,LayerMask.GetMask("Enemy"));
+        GameObject explosionFlower = Instantiate(explosionFlowerPrefab, transform.position, Quaternion.identity);
+        audioManager.PlaySFX("explosion");
+        
+        foreach (RaycastHit hitObj in rayHits)
+        {
+            hitObj.transform.GetComponent<Enemy>().currentHp -= damage;
+        }
+
+        Destroy(gameObject);
+        yield break;
     }
 }
